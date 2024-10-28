1e3dcbd681001de3171ad406fc7b72fdf4849ade
Maxim Bezrukavnikov <dmntr@rambler.ru>
2024-10-28 08:37:22 +0300

    Update hhUpdater.user.js

--- hhUpdater.user.js
+++ hhUpdater.user.js
@@ -3,7 +3,7 @@
 // @namespace    hhUpdater
 // @description  headhunter.ru
 // @icon         https://i.hh.ru/logos/svg/hh.ru__min_.svg
-// @version      0.1.6
+// @version      0.1.8
 // @author       DEMENT0R
 // @downloadURL  https://github.com/DEMENT0R/hhUpdater/raw/master/hhUpdater.user.js
 // @updateURL    https://github.com/DEMENT0R/hhUpdater/raw/master/hhUpdater.user.js
@@ -15,18 +15,19 @@
 
 (function() {
     'use strict';
-
-    const button = document.createElement('button');
-    button.innerText = 'Upp All';
-    button.id = 'upp-all-button';
-    button.addEventListener('click', () => {
-        var xpath = "//span[text()='Поднять в поиске']";
-        var matchingElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-        if (matchingElement) {
-            matchingElement.click();
-        } else {
-            alert('Oops!');
-        }
-    })
-    document.body.prepend(button);
+    const timerId = setInterval(() => {
+        upResume();
+    }, 5000);
 })();
+
+function upResume(warn = false){
+    var xpath = "//span[text()='Поднять в поиске']";
+    var matchingElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    if (matchingElement) {
+        matchingElement.click();
+    } else {
+        if (warn){
+            alert('Buttons not found!');
+        }
+    }
+}
